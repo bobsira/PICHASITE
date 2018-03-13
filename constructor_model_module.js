7b640d7c95767fbf7a9654c7276a9dc3b28a8b41
bobsira <sbobfitz2@gmail.com>
2018-03-13 13:27:02 +0300

    The Constructor Model

---
+++ constructor_model_module.js
@@ -0,0 +1,10 @@
+function ABC () {
+    this.varA = 10;
+    this.varB = 20;
+    this.functionA = function (var1, var2) {
+        console.log(var1 + " " + var2);
+    }
+}
+
+
+module.exports = ABC;
---
+++ constructor_model_test.js
@@ -0,0 +1,3 @@
+var abc = require('./constructor_model_module.js');
+var obj = new abc();
+obj.functionA(1, 2);
